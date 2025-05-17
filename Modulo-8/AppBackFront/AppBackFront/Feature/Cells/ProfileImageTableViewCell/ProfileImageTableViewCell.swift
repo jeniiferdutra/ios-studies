a784b152502b0ce9deaee59f4dac6b295e67347e
jenifer rocha <jeniferdutra2000@icloud.com>
2025-05-17 01:14:37 -0300

    feat: criação da tela de perfil (Profile)

---
+++ Modulo-8/AppBackFront/AppBackFront/Feature/Cells/ProfileImageTableViewCell/ProfileImageTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileImageTableViewCell.swift
+//  AppBackFront
+//
+//  Created by Jenifer Rocha on 17/05/25.
+//
+
+import UIKit
+
+class ProfileImageTableViewCell: UITableViewCell {
+    
+    static let identifier: String = String(describing: ProfileImageTableViewCell.self)
+    
+    lazy var screen: ProfileImageTableViewCellScreen = {
+        let view = ProfileImageTableViewCellScreen()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addViews()
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addViews() {
+        contentView.addSubview(screen)
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            
+            screen.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            screen.leadingAnchor.constraint(equalTo: leadingAnchor),
+            screen.trailingAnchor.constraint(equalTo: trailingAnchor),
+            screen.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+}
---
+++ Modulo-8/AppBackFront/AppBackFront/Feature/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewModel.swift
+//  AppBackFront
+//
+//  Created by Jenifer Rocha on 17/05/25.
+//
+
+import UIKit
+
+class ProfileViewModel: NSObject {
+
+}
