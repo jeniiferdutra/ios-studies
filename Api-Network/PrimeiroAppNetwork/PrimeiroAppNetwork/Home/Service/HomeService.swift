9fa2b3130436bd346686492ee4a00c6ece9bd6f0
jenifer rocha <jeniferdutra2000@icloud.com>
2025-05-20 21:58:45 -0300

    feat: model criada com base no response da API

--- Api-Network/PrimeiroAppNetwork/PrimeiroAppNetwork/Home/Service/HomeService.swift
+++ Api-Network/PrimeiroAppNetwork/PrimeiroAppNetwork/Home/Service/HomeService.swift
@@ -10,7 +10,35 @@ import UIKit
 class HomeService {
     
     func getPersonList() {
+        let urlString: String = "https://run.mocky.io/v3/78cc1974-0b38-4f8e-9726-4bc3cb544bc0"
         
+        guard let url: URL = URL(string: urlString) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error { // tirando a opcionalidade do erro
+                print("ERROR \(#function) Detalhe do erro: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data else {
+                
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                
+                return
+            }
+            
+//            do {
+//                let person: Person try 
+//            } catch <#pattern#> {
+//                <#statements#>
+//            }
+        }
+        task.resume()
     }
-
+    
 }
