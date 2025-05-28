e2243236babc5e8eab02410a593f3342861ffa7e
jenifer rocha <jeniferdutra2000@icloud.com>
2025-05-28 19:23:23 -0300

    feat: criação da class Home MVC

--- Arquitetura/SwiftArquiteturas/SwiftArquiteturas/Source/HomeVC.swift
+++ Arquitetura/SwiftArquiteturas/SwiftArquiteturas/Source/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  SwiftArquiteturas
 //
 //  Created by Jenifer Rocha on 26/05/25.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
