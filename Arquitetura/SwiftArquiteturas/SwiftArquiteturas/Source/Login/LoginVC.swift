f363ad22caf7892e9ee0bffba0be40e1d547dde0
jenifer rocha <jeniferdutra2000@icloud.com>
2025-05-29 19:53:43 -0300

    feat: implementação das actions de login e register MVP

---
+++ Arquitetura/SwiftArquiteturas/SwiftArquiteturas/Source/Login/LoginVC.swift
@@ -0,0 +1,32 @@
+//
+//  LoginVC.swift
+//  SwiftArquiteturas
+//
+//  Created by Jenifer Rocha on 29/05/25.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func tappedLoginButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func tappedRegisterButton(_ sender: UIButton) {
+    }
+    
+
+}
---
+++ Arquitetura/SwiftArquiteturas/SwiftArquiteturas/Source/Register/RegisterVC.swift
@@ -0,0 +1,33 @@
+//
+//  RegisterVC.swift
+//  SwiftArquiteturas
+//
+//  Created by Jenifer Rocha on 29/05/25.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func tappedRegisterButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func tappedBackButton(_ sender: UIButton) {
+    }
+    
+    
+}
