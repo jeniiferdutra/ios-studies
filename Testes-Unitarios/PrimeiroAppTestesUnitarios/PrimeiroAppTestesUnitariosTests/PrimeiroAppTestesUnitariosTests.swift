ade7eb2d48a6228a17062a38f1c6ee6a28f3ad86
jenifer rocha <jeniferdutra2000@icloud.com>
2025-08-07 00:13:09 -0300

    test: cria o segundo teste unitário da aplicação

--- Testes-Unitarios/PrimeiroAppTestesUnitarios/PrimeiroAppTestesUnitariosTests/PrimeiroAppTestesUnitariosTests.swift
+++ Testes-Unitarios/PrimeiroAppTestesUnitarios/PrimeiroAppTestesUnitariosTests/PrimeiroAppTestesUnitariosTests.swift
@@ -12,25 +12,19 @@ final class PrimeiroAppTestesUnitariosTests: XCTestCase {
     
     var vc: ViewController!
 
-    override func setUpWithError() throws { // Faz parte do ciclo de vida dos testes no XCTest. ele é chamado automaticamente antes de cada teste, como uma etapa do ciclo.
-        // Imagina que você vai fazer vários testes com uma calculadora. Antes de cada teste, você precisa ligar a calculadora e zerar a memória.
+    override func setUpWithError() throws {
         vc = ViewController()
-        print("1") // comeca aqui, depois do testPrimeiraFuncao() passa por aqui de novo
+        print("1")
     }
 
     override func tearDownWithError() throws {
-        //Ele serve para limpar o que você preparou no setUpWithError() — tipo "desligar a luz e arrumar a bagunça" depois do teste.
         vc = nil
-        print("3") // encerra aqui, qnd terminar o testSegundaFuncao(), vai encerrar aqui de novo
-        
     }
     
     func testPrimeiraFuncao() {
-        print("2") // inicia o primeiro teste
-        //validacao
+        print("2")
+        var valorTotal = 10 + 10
+        XCTAssertEqual(20, valorTotal)
     }
 
-    func testSegundaFuncao() {
-        print("2") //
-    }
 }
