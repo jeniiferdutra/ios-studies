3c98f29a19f5d7913a837980af582b588cdce375
jenifer rocha <jeniferdutra2000@icloud.com>
2025-08-07 00:03:34 -0300

    test: cria o primeiro teste unitário da aplicação

--- Testes-Unitarios/PrimeiroAppTestesUnitarios/PrimeiroAppTestesUnitariosTests/PrimeiroAppTestesUnitariosTests.swift
+++ Testes-Unitarios/PrimeiroAppTestesUnitarios/PrimeiroAppTestesUnitariosTests/PrimeiroAppTestesUnitariosTests.swift
@@ -15,25 +15,22 @@ final class PrimeiroAppTestesUnitariosTests: XCTestCase {
     override func setUpWithError() throws { // Faz parte do ciclo de vida dos testes no XCTest. ele é chamado automaticamente antes de cada teste, como uma etapa do ciclo.
         // Imagina que você vai fazer vários testes com uma calculadora. Antes de cada teste, você precisa ligar a calculadora e zerar a memória.
         vc = ViewController()
+        print("1") // comeca aqui, depois do testPrimeiraFuncao() passa por aqui de novo
     }
 
     override func tearDownWithError() throws {
         //Ele serve para limpar o que você preparou no setUpWithError() — tipo "desligar a luz e arrumar a bagunça" depois do teste.
-    }
-
-    func testExample() throws {
-        // verifica se uma parte do seu código está funcionando corretamente.
+        vc = nil
+        print("3") // encerra aqui, qnd terminar o testSegundaFuncao(), vai encerrar aqui de novo
+        
     }
     
     func testPrimeiraFuncao() {
-        
+        print("2") // inicia o primeiro teste
+        //validacao
     }
 
-    func testPerformanceExample() throws { // Por ora, nao vamos usar
-        // É um teste de performance, usado para medir quanto tempo um trecho do código leva para rodar.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSegundaFuncao() {
+        print("2") //
     }
-
 }
