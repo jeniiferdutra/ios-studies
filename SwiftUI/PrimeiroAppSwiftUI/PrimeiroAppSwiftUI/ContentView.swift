5900f4539b099fbc92e9d0b5e2544472fb85951f
jenifer rocha <jeniferdutra2000@icloud.com>
2025-08-19 20:40:39 -0300

    feat: cria projeto inicial em SwiftUI com ContentView

---
+++ SwiftUI/PrimeiroAppSwiftUI/PrimeiroAppSwiftUI/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  PrimeiroAppSwiftUI
+//
+//  Created by Jenifer Rocha on 19/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome!!")
+    }
+}
+
+// É usado apenas no Xcode para mostrar uma pré-visualização dessa View. Ele não faz parte do app final, serve só para o desenvolvedor ver no canvas
+#Preview {
+    ContentView()// Aqui a View ContentView é renderizada na prévia
+}
