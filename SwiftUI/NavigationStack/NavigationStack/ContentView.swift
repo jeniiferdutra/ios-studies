8abcae1b98d7da6fd876575f677a9d2f7f76ca44
jenifer rocha <jeniferdutra2000@icloud.com>
2025-08-27 20:38:47 -0300

    feat: adiciona conceitos iniciais de NavigationStack em SwiftUI

---
+++ SwiftUI/NavigationStack/NavigationStack/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  NavigationStack
+//
+//  Created by Jenifer Rocha on 27/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        //MARK: NavigationStack
+        
+        NavigationStack {
+            VStack(spacing: 10.0) {
+                NavigationLink("Ir para tela 1") {
+                    Text("Sou a tela 1")
+                }
+                NavigationLink("Ir para tela 2") {
+                    Text("Sou a tela 2")
+                }
+            }
+        }
+        
+        //MARK: NavigationStack Destination
+        
+        NavigationStack {
+            VStack(spacing: 10.0) {
+                NavigationLink("Ir para tela 1", value: "Sou a tela 1")
+                NavigationLink("Ir para tela 2", value: Color.red)
+            }
+            .navigationDestination(for: String.self) { value in
+                Text(value)
+            }
+            .navigationDestination(for: Color.self) { value in
+                ZStack {
+                    value
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
