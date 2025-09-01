571b3680f2b4a1c43cd28225145583a0325b842c
jenifer rocha <jeniferdutra2000@icloud.com>
2025-09-01 20:51:00 -0300

    feat: adiciona exemplo de TabView em SwiftUI

---
+++ SwiftUI/TabViewApp/TabViewApp/TabViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewAppApp.swift
+//  TabViewApp
+//
+//  Created by Jenifer Rocha on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TabViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
