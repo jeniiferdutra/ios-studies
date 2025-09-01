7cd3fdb363f100729af1bf963c0d0c7ade1bf898
jenifer rocha <jeniferdutra2000@icloud.com>
2025-09-01 20:29:56 -0300

    feat: adiciona BottomSheet de navegação em SwiftUI

---
+++ SwiftUI/BottomSheet/BottomSheet/BottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetApp.swift
+//  BottomSheet
+//
+//  Created by Jenifer Rocha on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
