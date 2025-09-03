30b6069088b1b7d81f823f5e4dac28fec4960851
jenifer rocha <jeniferdutra2000@icloud.com>
2025-09-03 00:16:11 -0300

    feat: adiciona exemplo de ScrollView em SwiftUI

---
+++ SwiftUI/ScrollView/ScrollView/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  ScrollView
+//
+//  Created by Jenifer Rocha on 02/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVStack(spacing: 15.0) {
+                    ForEach(0..<26) { index in
+                        Text("Posicao -> \(index)")
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 45)
+                            .background(Color.green)
+                    }
+                }
+            }
+            ScrollView(.horizontal, showsIndicators: false) { // remover a barra de scroll
+                LazyHStack(spacing: 15.0) {
+                    ForEach(0..<26) { index in
+                        Text("Posicao -> \(index)")
+                            .frame(height: 45)
+                            .background(Color.blue)
+                    }
+                }
+            }.frame(height: 60)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
