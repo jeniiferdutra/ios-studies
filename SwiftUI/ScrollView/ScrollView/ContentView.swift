f3686d9f482640346b7ace8e7a54790b3196a66c
jenifer rocha <jeniferdutra2000@icloud.com>
2025-09-03 22:05:56 -0300

    feat: implementação avançada de seleção de itens na ScrollView em SwiftUI

--- SwiftUI/ScrollView/ScrollView/ContentView.swift
+++ SwiftUI/ScrollView/ScrollView/ContentView.swift
@@ -7,7 +7,22 @@
 
 import SwiftUI
 
+struct Person: Identifiable {
+    let id = UUID()
+    let name: String
+    let age: Int
+}
+
 struct ContentView: View {
+    
+    @State var selectedPosition: UUID? = nil
+    
+    var list: [Person] = [
+        Person(name: "Jenifer", age: 25),
+        Person(name: "Guilherme", age: 31),
+        Person(name: "Duke", age: 12),
+    ]
+    
     var body: some View {
         VStack {
             ScrollView(.vertical, showsIndicators: false) {
@@ -39,12 +54,22 @@ struct ContentView: View {
             }
             ScrollView(.horizontal, showsIndicators: false) { // remover a barra de scroll
                 LazyHStack(spacing: 15.0) {
-                    ForEach(0..<26) { index in
-                        Text("Posicao -> \(index)")
+//                    ForEach(0..<26) { index in
+//                        Text("Posicao -> \(index)")
+//                            .frame(height: 45)
+//                            .background(selectedPosition == index ? Color.red : Color.blue)
+//                            .onTapGesture {
+//                                print("Clicou na posicao \(index)")
+//                                selectedPosition = index
+//                            }
+//                    }
+                    
+                    ForEach(list) { person in
+                        Text("Nome -> \(person.name)")
                             .frame(height: 45)
-                            .background(Color.blue)
+                            .background(selectedPosition == person.id ? Color.red : Color.blue)
                             .onTapGesture {
-                                print("Clicou na posicao \(index)")
+                                selectedPosition = person.id
                             }
                     }
                 }
