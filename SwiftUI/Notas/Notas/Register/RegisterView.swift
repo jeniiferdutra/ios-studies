094094b99984ae170d48cdb8051ea7def620eece
jenifer rocha <jeniferdutra2000@icloud.com>
2025-09-12 22:53:51 -0300

    feat: cria tela de Register com campos e layout inicial

--- SwiftUI/Notas/Notas/Register/RegisterView.swift
+++ SwiftUI/Notas/Notas/Register/RegisterView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var email: String = ""
+    @State var senha: String = ""
+    @State var confirmeSenha: String = ""
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.backgroundColor.ignoresSafeArea()
+            VStack(spacing: 25.0) {
+                
+                Text("Cadastrar")
+                    .font(.system(size: 55, weight: .bold))
+                    .foregroundStyle(.white)
+                    .padding(.top, 15)
+                    .padding(.bottom, 50)
+                                
+                Group {
+                    TextField("", text: $email, prompt: Text("Email").foregroundStyle(.white))
+                    SecureField("", text: $senha, prompt: Text("Senha").foregroundStyle(.white))
+                    SecureField("", text: $confirmeSenha, prompt: Text("Confirme a Senha").foregroundStyle(.white))
+                }
+                .frame(height: 40)
+                .padding(7)
+                .overlay(content: {
+                    RoundedRectangle(cornerRadius: 8).stroke(Color.pinkColor, lineWidth: 2)
+                })
+                .foregroundStyle(.white)
+                .autocorrectionDisabled() // autocorrecao
+                .textInputAutocapitalization(.never) // primeira letra maiuscula
+                
+                Spacer()
+                
+                Button {
+                    print("tapped")
+                } label: {
+                    Text("Cadastrar")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 45)
+                        .foregroundStyle(.white)
+                        .font(.system(size: 18, weight: .bold))
+                        .background(isDisabledRegisterButton ? Color.pinkColor.opacity(0.6) : Color.pinkColor)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                .disabled(isDisabledRegisterButton)
+                
+            }
+            .padding(.horizontal, 20)
+            .padding(.bottom, 50)
+        }
+    }
+    
+    var isDisabledRegisterButton: Bool { // verificar se sao empty
+        return email.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || senha.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || confirmeSenha.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
     }
 }
 
