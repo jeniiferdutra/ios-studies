bb24b53411c4f1dd60a6b0cce17762fa0f3be15b
jenifer rocha <jeniferdutra2000@icloud.com>
2025-10-09 23:03:15 -0300

    feat: implementa salvamento local de notas com UserDefaults

--- SwiftUI/Notas/Notas/Notes/ViewModel/NotesViewModel.swift
+++ SwiftUI/Notas/Notas/Notes/ViewModel/NotesViewModel.swift
@@ -9,6 +9,23 @@ import Foundation
 
 class NotesViewModel: ObservableObject {
     
-    @Published var notes: [Note] = [Note(title: "Titulo", content: "Descricao do conteudo")]
+    @Published var notes: [Note] = [] {
+        didSet { // toda vez q fazer att no notes, eu quero que salve em saveNotes()
+            saveNotes()
+        }
+    }
+    
+    init() { // criar um construtor
+        guard let data = UserDefaults.standard.data(forKey: "notes") else { return } // pegar info da chave "notes
+        if let getNotes = try? JSONDecoder().decode([Note].self, from: data) { // decodifica data em array de Note
+            self.notes = getNotes
+        }
+    }
+    
+    func saveNotes() {
+        if let encoded = try? JSONEncoder().encode(notes) {
+            UserDefaults.standard.setValue(encoded, forKey: "notes")
+        }
+    }
     
 }
