42db16d4fc6c1940a27e95c0a194e51ebf3e74ea
jenifer rocha <jeniferdutra2000@icloud.com>
2025-10-14 20:06:55 -0300

    feat: adiciona ToolbarItem ao projeto

--- SwiftUI/BackFrontPosts/BackFrontPosts/HomeView/HomeView.swift
+++ SwiftUI/BackFrontPosts/BackFrontPosts/HomeView/HomeView.swift
@@ -9,7 +9,55 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Tela Home !")
+        NavigationStack {
+            ZStack {
+                Color(red: 237/255, green: 237/255, blue: 237/255)
+                Text("Tela Home !")
+            }
+            .toolbarTitleDisplayMode(.inline)
+            .toolbar(content : {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("bf_logo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 100)
+                    
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack {
+                        Button(action: {
+                            print("go search")
+                        }, label: {
+                            Image("search")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 25, height: 25)
+                        })
+                        
+                        Button(action: {
+                            print("go search")
+                        }, label: {
+                            ZStack {
+                                Image("search")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 25, height: 25)
+                                
+                                Text("3")
+                                    .font(.system(size: 13, weight: .bold))
+                                    .frame(width: 18, height: 18)
+                                    .background(Color.red)
+                                    .foregroundStyle(.white)
+                                    .clipShape(Circle())
+                                    .offset(x: 9, y: -9)
+                            }
+                        })
+                    }
+                    
+                }
+            })
+        }
     }
 }
 
