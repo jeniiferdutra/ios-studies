7ce96a3f217b0dc6b8ae2ec90bdbb5a50d0290df
jenifer rocha <jeniferdutra2000@icloud.com>
2025-10-22 23:41:58 -0300

    feat: finaliza desenvolvimento do Story

--- SwiftUI/BackFrontPosts/BackFrontPosts/StoryView/StoryView.swift
+++ SwiftUI/BackFrontPosts/BackFrontPosts/StoryView/StoryView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct StoryView: View {
     
-    var story: StoryData
+    @State var story: StoryData
     
     private let gradient = LinearGradient(gradient: Gradient(colors: [Color.yellow, Color(red: 252/255, green: 175/255, blue: 69/255), Color.red, Color(red:225/255, green: 48/255,blue: 108/255), Color(red:193/255, green: 53/255, blue: 132/255),Color(red:131/255, green: 58/255, blue: 180/255)]), startPoint: .bottomLeading, endPoint: .topTrailing)
-
+    
     
     var body: some View {
-        VStack {
+        VStack(spacing: 8)  {
             ZStack {
                 Circle().stroke(gradient, style: StrokeStyle(lineWidth: 2.5, lineCap: .round))
                     .frame(width: 72)
@@ -34,6 +34,11 @@ struct StoryView: View {
                     
                 }
             }
+            Text(story.name)
+                .lineLimit(1)// n ter quebra de linha
+                .font(Font.system(size: 12, weight: .regular))
+                .foregroundStyle(.primary)
+            
         }
     }
 }
