188b8f7adc3be513eec41776b7e89f9d962a0bf6
jenifer rocha <jeniferdutra2000@icloud.com>
2025-10-31 21:10:52 -0300

    feat: adiciona animação de like ao Post

--- SwiftUI/BackFrontPosts/BackFrontPosts/PostView/PostView.swift
+++ SwiftUI/BackFrontPosts/BackFrontPosts/PostView/PostView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct PostView: View {
     
     @State var post: PostData
+    @State var isLikeAnimation: Bool = false
     
     var body: some View {
         VStack {
@@ -33,14 +34,48 @@ struct PostView: View {
                         .scaledToFill()
                         .frame(width: 30, height: 30)
                 }
-
+                
             }
             .padding(.vertical, 10)
             .padding(.horizontal, 12)
+            
+            ZStack {
+                if !post.isVideo { // se nao for video
+                    Image(post.postImage ?? "error")
+                        .resizable()
+                        .scaledToFit()
+                        .onTapGesture(count: 2, perform: { // acao do like ao clicar 2 vezes
+                            tappedLike()
+                        })
+                } else {
+                    
+                }
+                Image("white-heart")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 170, height: 170)
+                    .scaleEffect(isLikeAnimation ? 1 : 0) // o coracao aparece e some
+                    .opacity(isLikeAnimation ? 1 : 0) // opacidade do coracao
+                    .animation(.spring, value: isLikeAnimation) // disparar a animacao
+            }
+            
         }
     }
+    
+    func tappedLike() {
+        isLikeAnimation = true
+        post.isLiked = true
+    }
+    
+    func hideAnimation() { // coracao sumir depois do clique
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+            withAnimation {
+                isLikeAnimation = false
+            }
+        }
+    }
+    
 }
-
 #Preview {
     PostView(post: PostMock[1])
 }
